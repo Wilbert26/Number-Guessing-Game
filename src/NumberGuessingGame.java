ea9ae5055d1bec0024f489b9a0398bdc52d40534
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-09-25 08:15:24 -0400

    added new code

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -6,18 +6,39 @@ public class NumberGuessingGame {
       Scanner keyboard;
      int testing;
       int pickednumber;
+      String guess;
 
       keyboard=new Scanner(System.in);
 
 
       pickednumber=(int)(Math.random()*100);
 
+      System.out.println(" pick a number from 1-100");
+      testing=keyboard.nextInt();
+      if (testing<pickednumber) {
+          System.out.println("too low");
+      }
+
+else if (testing>pickednumber){
+ System.out.println("too high");
+      }
+
+       if (testing==pickednumber){
+          System.out.println("you win would you like to try again");
+       }
+
+      System.out.println("try a different number");
+      testing=keyboard.nextInt();
+
+      System.out.println("last try");
+      testing=keyboard.nextInt();
 
 
       System.out.println(pickednumber);
-if (testing<54) {
-    System.out.println();
-}
+
+
+
+
 
 
 
