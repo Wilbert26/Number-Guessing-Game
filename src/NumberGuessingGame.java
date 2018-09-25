d2a4d70cd663d1fcc694d6098acddd5aff0f05ba
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-09-25 07:29:36 -0400

    started

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class NumberGuessingGame {
 
   public static void main(String[]args){
+      Scanner keyboard;
+     int testing;
+      int pickednumber;
+
+      keyboard=new Scanner(System.in);
+
+
+      pickednumber=(int)(Math.random()*100);
 
 
 
+      System.out.println(pickednumber);
+if (testing<54) {
+    System.out.println();
+}
 
 
 
